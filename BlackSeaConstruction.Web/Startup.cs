286508bd7bd3080ead7c540d33c5165eb778fe23
Nikita Dermenzhi <nikita.dermenzi@gmail.com>
2019-06-13 18:15:48 -0400

    Changed hotspot with and routing

--- BlackSeaConstruction.Web/Startup.cs
+++ BlackSeaConstruction.Web/Startup.cs
@@ -55,7 +55,7 @@ namespace BlackSeaConstruction.Web
 
                 routes.MapRoute(
                    name: "default",
-                   template: "{controller=Home}/{action=Index}");
+                   template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
